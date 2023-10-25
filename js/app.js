c95018ec6dd935746cd32cdce616e5e8e6be883d
aoffer123 <aoffer@oakland.edu>
2023-10-25 10:56:42 -0400

    second commit

---
+++ js/app.js
@@ -0,0 +1,48 @@
+
+
+
+
+function showImgA() {
+    let imageA = document.querySelector(".imageA");
+    imageA.style.visibility = "visible";
+    let hide_aimg = document.querySelector(".hide_aimg");
+    hide_aimg.style.visibility = "visible";
+}
+
+function hideImgA() {
+    let imageA = document.querySelector(".imageA");
+    imageA.style.visibility = "hidden";
+    let hide_aimg = document.querySelector(".hide_aimg");
+    hide_aimg.style.visibility = "hidden";
+
+}
+
+function showImgB() {
+    let imageB = document.querySelector(".imageB");
+    imageB.style.visibility = "visible";
+    let hide_bimg = document.querySelector(".hide_bimg");
+    hide_bimg.style.visibility = "visible";
+}
+
+function hideImgB() {
+    let imageB = document.querySelector(".imageB");
+    imageB.style.visibility = "hidden";
+    let hide_bimg = document.querySelector(".hide_bimg");
+    hide_bimg.style.visibility = "hidden";
+
+}
+
+function showImgC() {
+    let imageC = document.querySelector(".imageC");
+    imageC.style.visibility = "visible";
+    let hide_cimg = document.querySelector(".hide_cimg");
+    hide_cimg.style.visibility = "visible";
+}
+
+function hideImgC() {
+    let imageC = document.querySelector(".imageC");
+    imageC.style.visibility = "hidden";
+    let hide_cimg = document.querySelector(".hide_cimg");
+    hide_cimg.style.visibility = "hidden";
+
+}
